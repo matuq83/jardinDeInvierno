2e3a7319479e62cb10e584452374a588f752e06f
matuq83 <mathiasq.mq@gmail.com>
2024-11-12 21:33:45 -0300

    nueva version para que se pueda rederizar en netlify

--- script.js
+++ script.js
@@ -6,53 +6,6 @@ function showSection(sectionId) {
   });
 
   // Show the selected section
-  document.getElementById(sectionId).style.display = "block";
-}
-
-function searchProducts() {
-  const input = document.getElementById("searchInput").value.toLowerCase();
-  const items = document.querySelectorAll(".item");
-
-  items.forEach((item) => {
-    const itemName = item
-      .querySelector(".details h3")
-      .textContent.toLowerCase();
-    if (itemName.includes(input)) {
-      item.style.display = "flex";
-    } else {
-      item.style.display = "none";
-    }
-  });
-}
-
-function changeLanguage(language) {
-  // Implement language change logic here
-  alert("Language changed to: " + language);
-}
-
-window.onscroll = function () {
-  const backToTopButton = document.getElementById("backToTop");
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    backToTopButton.style.display = "block";
-  } else {
-    backToTopButton.style.display = "none";
-  }
-};
-
-function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-*/
-
-
-function showSection(sectionId) {
-  // Hide all sections
-  document.querySelectorAll(".section").forEach((section) => {
-    section.style.display = "none";
-  });
-
-  // Show the selected section
   const selectedSection = document.getElementById(sectionId);
   selectedSection.style.display = "block";
 
@@ -98,3 +51,5 @@ function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+*/
